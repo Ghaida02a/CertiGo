e84f46d2bfa20fb8c141f6d4992ccb1da9ff4839
Ghaida02a <ggmskaj@gmail.com>
2026-01-04 11:08:54 +0400

    update Constants.java

--- CertiGo/CertiGo/src/main/java/com/codeline/CertiGo/Helper/Constants.java
+++ CertiGo/CertiGo/src/main/java/com/codeline/CertiGo/Helper/Constants.java
@@ -1,5 +1,13 @@
 package com.codeline.CertiGo.Helper;
 
 public class Constants {
-    
+    // HTTP Status Codes
+    public static final int HTTP_STATUS_IS_NULL= 422;
+
+    //User Create Requested
+    public static final String USER_USER_NAME_NOT_VALID = "Username is not valid";
+    public static final String USER_EMAIL_NOT_VALID = "User email is not valid";
+    public static final String USER_PASSWORD_NOT_VALID = "User password is not valid";
+    public static final String USER_ROLE_NOT_VALID = "User role is not valid";
+
 }
