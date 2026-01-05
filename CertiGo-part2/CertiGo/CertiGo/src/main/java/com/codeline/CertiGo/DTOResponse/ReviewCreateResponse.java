4361a76074ee7cf56f64f417595d2e58e08771ee
Shaima Al-Nabhani <shaimaalnabhani@gmail.com>
2026-01-05 17:03:15 +0400

    Testing APIs

---
+++ CertiGo-part2/CertiGo/CertiGo/src/main/java/com/codeline/CertiGo/DTOResponse/ReviewCreateResponse.java
@@ -0,0 +1,29 @@
+package com.codeline.CertiGo.DTOResponse;
+
+import com.codeline.CertiGo.Entity.Review;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewCreateResponse {
+
+    private Integer id;
+    private String reviewerName;
+    private String comments;
+    private Integer rating;
+
+    public static ReviewCreateResponse convertToReviewResponse(Review entity) {
+        ReviewCreateResponse response = ReviewCreateResponse.builder()
+                .id(entity.getId())
+                .reviewerName(entity.getReviewerName())
+                .comments(entity.getComments())
+                .rating(entity.getRating())
+                .build();
+        return response;
+    }
+}
